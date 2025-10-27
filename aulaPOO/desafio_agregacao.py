63f2c714dec51f135f4a5db3d362ad22e39eba51
camillo <lazarettieb@gmail.com>
2025-10-27 07:29:44 -0300

    exercicioteste agregacao

--- aulaPOO/desafio_agregacao.py
+++ aulaPOO/desafio_agregacao.py
@@ -1,2 +1,38 @@
 
 
+
+
+
+class Livros:
+    def __init__(self, titulo, autor, paginas, disponibilidade):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+        self.disponibilidade = disponibilidade
+
+class Biblioteca:
+    def __init__(self):
+        self.livros = []
+
+    def adicionar_livros(self, livro):
+        self.livros.append(livro)
+
+    def listar_livros(self):
+        counter = 1
+        for livro in self.livros:
+            print(f'O {counter}° livro se chama {livro.titulo}, do autor {livro.autor} e possui {livro.paginas} páginas!')
+            if livro.disponibilidade:
+                print('Livro disponível para venda')
+            else:
+                print('Livro indisponível')
+            counter += 1
+
+l1 = Livros('A Serpente',  'Eduarda Ferreira', 512, False)
+l2 = Livros('Sai Azar', 'Luide', 15, True)
+
+
+biblioteca = Biblioteca()
+biblioteca.adicionar_livros(l1)
+biblioteca.adicionar_livros(l2)
+
+biblioteca.listar_livros()
